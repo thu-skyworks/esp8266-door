315ec64ac1dd9a5af06513df125ef4a260b0f03a
zhang <zz593141477@gmail.com>
2018-10-24 22:50:05 +0800

    scripts

--- generate_secrets.sh
+++ generate_secrets.sh
@@ -10,3 +10,19 @@ const char *mqtt_broker = "$PRJ_CFG_BROKER"; // MQTT Broker
 const char *mqtt_user = "$PRJ_CFG_MQTT_USER";               // MQTT Username
 const char *mqtt_password = "$PRJ_CFG_MQTT_PASSWD"; // MQTT Password
 EOF
+
+cat >config.py <<EOF
+MQTT_BROKER = "$PRJ_CFG_BROKER" # MQTT Broker
+MQTT_USER = "$PRJ_CFG_MQTT_USER"               # MQTT Username
+MQTT_PASSWORD = "$PRJ_CFG_MQTT_PASSWD" # MQTT Password
+
+DBNAME = "$PRJ_CFG_DBNAME"
+DBUSER = "$PRJ_CFG_DBUSER"
+DBPASS = "$PRJ_CFG_DBPASS"
+
+LDAP_URI  = "$PRJ_CFG_LDAP_URI"
+LDAP_USER = "$PRJ_CFG_LDAP_USER"
+LDAP_PASS = "$PRJ_CFG_LDAP_PASS"
+
+
+EOF
